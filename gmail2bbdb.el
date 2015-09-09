acf0e3b6b5d38576451f47fd5263b34402b28288
Chen Bin <chenbin.sh@gmail.com>
2015-09-10 09:57:05 +1000

    README updated

--- gmail2bbdb.el
+++ gmail2bbdb.el
@@ -14,12 +14,13 @@
 ;;
 ;; Insert below code into ~/.emacs:
 ;; (add-to-list 'load-path "~/.emacs.d/lisp/")
-;; (autoload 'gmail2bbdb-import-file "gmail2bbdb")
+;; (autoload 'gmail2bbdb-import-file "gmail2bbdb" nil t nil)
 ;;
 
 ;;; Usage:
 ;;
-;; - At https://www.google.com/contacts, click "More -> Export -> vCard format -> Export", the file "contacts.vcf" will be downloaded.
+;; - At https://www.google.com/contacts, click "More -> Export -> vCard format -> Export"
+;;   the file "contacts.vcf" will be downloaded.
 ;; - Run command "M-x gmail2bbdb-import-file" and select contacts.vcf. ~/.bbdb will be created.
 ;;
 
