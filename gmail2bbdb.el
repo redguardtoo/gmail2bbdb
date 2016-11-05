599dc3f66477f53e98da7ac1bde99e7035fcfc43
Chen Bin <chenbin.sh@gmail.com>
2016-11-05 14:20:30 +1100

    remove duplicated records. option to exclude people without name

--- gmail2bbdb.el
+++ gmail2bbdb.el
@@ -4,7 +4,7 @@
 ;; Author: Chen Bin <chenbin.sh@gmail.com>
 ;; URL: http://github.com/redguardtoo/gmail2bbdb
 ;; Keywords: vcard bbdb email contact gmail
-;; Version: 0.0.4
+;; Version: 0.0.5
 
 ;; This file is not part of GNU Emacs.
 
@@ -30,37 +30,43 @@
 ;;   (autoload 'gmail2bbdb-import-file "gmail2bbdb" nil t nil)
 ;;
 ;; Usage:
-;;   - At https://www.google.com/contacts, click "More->Export->vCard format->Export".
-;;     The file "contacts.vcf" will be downloaded.
-;;   - Run command "M-x gmail2bbdb-import-file" and select contacts.vcf.
-;;     ~/.bbdb will be created.
+;; - Click "More->Export->vCard format->Export" at Google Contacts.
+;;   Download "contacts.vcf".
+;; - Run "M-x gmail2bbdb-import-file" and select contacts.vcf.
+;;   "~/.bbdb" is created.
+;; - Keep using BBDB.
 ;;
 
 ;;; Code:
 
 (defvar gmail2bbdb-bbdb-file "~/.bbdb"
-  "The full path of exported BBDB file.")
+  "Where BBDB file are exported.")
 
 (defvar gmail2bbdb-excluded-email-regex-list '("^noreply.*"
                                                "notify.*@disqus.net"
                                                ".*@noreply.github.com$"
                                                "reply.*@reply.github.com"
                                                "do-not-reply@stackoverflow.com")
-  "Email matching any regex from the list will NOT be exported")
+  "Email matching any regex of the list are NOT exported.")
+
+(defvar gmail2bbdb-exclude-people-without-name nil
+  "Exclude people without name.")
 
 (defun gmail2bbdb--is-valid-email (eml)
-  (let* ((i 0) excluded re)
+  (let* ((i 0) excluded)
     (while (and (not excluded)
                 (< i (length gmail2bbdb-excluded-email-regex-list)))
-      (setq re (nth i gmail2bbdb-excluded-email-regex-list))
-      (if (string-match re eml) (setq excluded t))
+      (if (string-match (nth i gmail2bbdb-excluded-email-regex-list)
+                        eml)
+          (setq excluded t))
       (setq i (1+ i)))
     (not excluded)))
 
 ;; ["Spolsky" "Joel" nil ("Spolsky Joel") nil nil nil ("spolsky@fogcreek.com") nil nil]
 (defun gmail2bbdb--extract-item (str)
-  (let* (lines fullname firstname lastname emails rlt eml)
-    (setq lines (split-string str "[\r\n]+"))
+  (let* ((lines (split-string str "[\r\n]+"))
+         fullname firstname lastname emails rlt eml)
+
     (dolist (l lines)
       (cond
        ((string-match "^FN:\\(.*\\)" l)
@@ -72,7 +78,10 @@
 
        ((string-match "TYPE=[A-Z]+:\\([^ @]+@[^ @]+\\)" l)
         (setq eml (match-string 1 l))
-        (if (gmail2bbdb--is-valid-email eml)
+        (if (and (or (not gmail2bbdb-exclude-people-without-name)
+                     (not (string= "" firstname))
+                     (not (string= "" lastname)))
+                 (gmail2bbdb--is-valid-email eml))
             (add-to-list 'emails eml)))))
     (when emails
       (setq rlt (vector firstname lastname nil (list fullname) nil nil nil emails nil nil))
@@ -84,8 +93,8 @@
 If VCARD-FILE is a wildcard, import each matching file.
 Existing BBDB records will be *overrided*."
   (interactive "FvCard file (wildcard could be used): ")
-  (let* (dst-file rlt)
-    (setq dst-file (file-truename gmail2bbdb-bbdb-file))
+  (let* ((dst-file (file-truename gmail2bbdb-bbdb-file))
+         rlt)
     (with-temp-buffer
       (insert-file-contents vcard-file)
       (goto-char (point-min))
@@ -98,15 +107,16 @@ Existing BBDB records will be *overrided*."
       (while (re-search-forward
               "^\\([[:alnum:]-]*\\.\\)?*BEGIN:VCARD[\n[:print:][:cntrl:]]*?\\(^\\([[:alnum:]-]*\\.\\)?END:VCARD\\)"
               nil t)
-        (let ((vcard (match-string 0)) item)
-          (setq item (gmail2bbdb--extract-item vcard))
-          (if item (setq rlt (concat rlt "\n" item))))))
+        (let* ((vcard (match-string 0))
+               (item (gmail2bbdb--extract-item vcard)))
+          (if item
+              (add-to-list 'rlt item)))))
 
     (if rlt
         (with-temp-buffer
           (insert ";; -*- mode: Emacs-Lisp; coding: utf-8; -*-\n")
           (insert ";;; file-format: 7\n")
-          (insert rlt)
+          (insert (mapconcat 'identity rlt "\n"))
           (write-file (file-truename gmail2bbdb-bbdb-file)))
       (message "No email found"))))
 
